ddc5462f7becdaf1417e6dc49f5dfed42a1e9003
Antonio Pantserno <apantserno@shutterstock.com>
2020-04-29 10:05:17 +0300

    added account Numbers for customers response

--- src/controllers/getCustomers.js
+++ src/controllers/getCustomers.js
@@ -17,13 +17,26 @@ export const getCustomers = async (req, res) => {
       return res.status(404).json({ message: 'Customers not found!' });
     }
 
-    const customers = customerDbEntries.map((customer, i) => ({
-      id: customer.user_guid,
-      customerName: `${customer.user_firstname} ${customer.user_lastname}`,
-      email: customer.user_email,
-      accountNumber: i,
-      lastLogin: customer.user_last_login
-    }));
+    const customers = await Promise.all(
+      customerDbEntries.map(async (customer) => {
+        const accountNumberEntries = await db
+          .select('storage_md_accounts_array')
+          .from('archive.storage')
+          .where('storage_user_guid', customer.user_guid);
+
+        const accountNumbers = accountNumberEntries
+          .map(({ storage_md_accounts_array }) => storage_md_accounts_array)
+          .filter(accountsArray => accountsArray)
+          .flat();
+
+        return {
+          id: customer.user_guid,
+          customerName: `${customer.user_firstname} ${customer.user_lastname}`,
+          email: customer.user_email,
+          accountNumbers,
+          lastLogin: customer.user_last_login
+        };
+      }));
 
     return res.status(200).json({ customers });
   } catch (error) {
